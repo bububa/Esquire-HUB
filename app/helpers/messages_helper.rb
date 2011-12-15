e2785765605830015f10ee70662f52d4c96f0b9b
Prof Syd Xu <thepeppersstudio@gmail.com>
2011-12-15 16:53:28 +0800

    add page for admin to manage messages

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,6 +2,9 @@ module MessagesHelper
   def outbox_path
     "#{root_url}messages/out/"
   end
+  def messages_all_path
+    "#{root_url}messages/all/"
+  end
   def message_delete_path(message)
     "#{root_url}message/delete/#{message.id}"
   end
